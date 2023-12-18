196e2dd080a832712b6b2d66f29ac01777987e78
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-12-18 12:41:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let ans = '#';
+
+	if(shortHex.charAt(0) === '#'){
+		shortHex = shortHex.substring(1, shortHex.length);
+	}
+	
+	let isCapital = 'A' >= shortHex.charAt(0) && shortHex.charAt(0) <= 'Z';
+
+	for(let i = 0; i < shortHex.length; i++){
+		ans = ans + shortHex.charAt(i) + shortHex.charAt(i);
+	}
+
+	return isCapital=== true ? ans.toUpperCase() : ans;
 };
 
 // Do not change the code below.
